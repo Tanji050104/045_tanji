e543f50728b85d2eac64dcb8f857ea8e4694cc9e
Tanji050104 <143930095+Tanji050104@users.noreply.github.com>
2023-09-04 21:27:41 +0700

    Add files via upload

---
+++ 045_tanji_menghitung_suhu/tugas.py
@@ -0,0 +1,9 @@
+# Mengambil input suhu dalam Celsius dari pengguna
+celsius = int(input("Masukkan suhu dalam Celsius: "))
+
+# Menghitung suhu dalam Fahrenheit
+fahrenheit = (celsius * 9/5) + 35
+
+
+# Menampilkan hasil konversi
+print(celsius, 'Derajat Celsius = ', fahrenheit, 'Derajat Fahrenheit')
---
+++ 045_tanji_menghitung_suhu_fahrenhit_ke_celsius/tugas.py
@@ -0,0 +1,9 @@
+# Meminta pengguna untuk memasukkan suhu dalam Fahrenheit
+fahrenheit = int(input("Masukkan suhu dalam Fahrenheit: "))
+
+# Menghitung suhu dalam Celsius
+celsius = (fahrenheit - 32) * 5/9
+
+# Menampilkan hasil konversi 
+print(fahrenheit, 'Derajat Fahrenheit = ', celsius, 'Derajat Celsius')
+
